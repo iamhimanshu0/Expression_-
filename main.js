61f419f8e27d53f5fc634a3ba5b8a390b18f1220
himanshu16998 <himanshutripathi16.ht@gmail.com>
2019-06-28 10:46:40 -0700

    Add Files

---
+++ main.js
@@ -0,0 +1,112 @@
+// Copyright (c) 2018 ml5
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+/* ===
+ml5 Example
+Image Classification using Feature Extraction with MobileNet. Built with p5.js
+This example uses a callback pattern to create the classifier
+=== */
+
+let featureExtractor;
+let classifier;
+let video;
+let loss;
+let sadImages = 0;
+let happyImages = 0;
+
+function setup() {
+  noCanvas();
+  // Create a video element
+  video = createCapture(VIDEO);
+  // Append it to the videoContainer DOM element
+  video.parent('videoContainer');
+  // Extract the already learned features from MobileNet
+  featureExtractor = ml5.featureExtractor('MobileNet', modelReady);
+  // Create a new classifier using those features and give the video we want to use
+  classifier = featureExtractor.classification(video, videoReady);
+  // Set up the UI buttons
+  setupButtons();
+}
+
+// A function to be called when the model has been loaded
+function modelReady() {
+  select('#modelStatus').html('Model Ready');
+  classifier.load('./model/model.json', function() {
+    select('#modelStatus').html('Custom Model Loaded!');
+  });
+}
+
+// A function to be called when the video has loaded
+function videoReady () {
+  select('#videoStatus').html('Video ready!');
+}
+
+// Classify the current frame.
+function classify() {
+  classifier.classify(gotResults);
+}
+
+// Buttons
+function setupButtons() {
+  // When the Happy button is pressed, add the current frame
+  // from the video with a label of "happy" to the classifier
+  buttonA = select('#happyButton');
+  buttonA.mousePressed(function() {
+    classifier.addImage('Happy');
+    select('#amountOfHappyImages').html(happyImages++);
+  });
+
+  // When the Sad button is pressed, add the current frame
+  // from the video with a label of "sad" to the classifier
+  buttonB = select('#sadButton');
+  buttonB.mousePressed(function() {
+    classifier.addImage('Sad');
+    select('#amountOfSadImages').html(sadImages++);
+  });
+
+  // Train Button
+  train = select('#train');
+  train.mousePressed(function() {
+    classifier.train(function(lossValue) {
+      if (lossValue) {
+        loss = lossValue;
+        select('#loss').html('Loss: ' + loss);
+      } else {
+        select('#loss').html('Done Training! Final Loss:   ' + loss);
+      }
+    });
+  });
+
+  // Predict Button
+  buttonPredict = select('#buttonPredict');
+  buttonPredict.mousePressed(classify);
+
+  // Save model
+  saveBtn = select('#save');
+  saveBtn.mousePressed(function() {
+    classifier.save();
+  });
+
+  // Load model
+  loadBtn = select('#load');
+  loadBtn.changed(function() {
+    classifier.load(loadBtn.elt.files, function(){
+      select('#modelStatus').html('Custom Model Loaded!');
+    });
+  });
+}
+
+// Show the results
+function gotResults(err, results) {
+  // Display any error
+  if (err) {
+    console.error(err);
+  }
+  if (results && results[0]) {
+    select('#result').html(results[0].label);
+    select('#confidence').html(results[0].confidence);
+    classify();
+  }
+}
